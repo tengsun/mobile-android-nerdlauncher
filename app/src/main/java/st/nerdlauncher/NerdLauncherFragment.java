b33a350e887774624d3b7f31136bb6a1024dbb25
Teng Sun <tengsun@cn.ibm.com>
2016-02-26 12:12:16 +0800

    start activity when click on the list item

--- app/src/main/java/st/nerdlauncher/NerdLauncherFragment.java
+++ app/src/main/java/st/nerdlauncher/NerdLauncherFragment.java
@@ -1,6 +1,7 @@
 package st.nerdlauncher;
 
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.os.Bundle;
@@ -9,6 +10,7 @@ import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.Collections;
@@ -59,5 +61,20 @@ public class NerdLauncherFragment extends ListFragment {
         };
 
         setListAdapter(adapter);
+    }
+
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id) {
+        ResolveInfo resolveInfo = (ResolveInfo) l.getAdapter().getItem(position);
+        ActivityInfo activityInfo = resolveInfo.activityInfo;
+
+        if (activityInfo == null) {
+            return;
+        }
+
+        Intent intent = new Intent(Intent.ACTION_MAIN);
+        intent.setClassName(activityInfo.applicationInfo.packageName, activityInfo.name);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
     }
 }
